74df701c2bad276ca93d374dd766b3453851a275
mathi4k2 <mathiforcado@gmail.com>
2023-09-15 17:49:31 -0400

    Se agrega lector de QR

--- qrscanner.js
+++ qrscanner.js
@@ -1,15 +1,75 @@
-const scanner = new Instascan.Scanner({ video: document.getElementById('scanner') });
-scanner.addListener('scan', function (content) {
-    // Cuando se escanea un código QR, se ejecuta esta función
-    const decodedData = decodeURIComponent(content);
-    document.getElementById('scan-result').textContent = `Datos decodificados: ${decodedData}`;
-});
+//crea elemento
+const video = document.createElement("video");
 
-// Iniciar el escáner
-Instascan.Camera.getCameras().then(cameras => {
-    if (cameras.length > 0) {
-        scanner.start(cameras[0]); // Usar la primera cámara encontrada
-    } else {
-        console.error('No se encontraron cámaras.');
-    }
-});
+//nuestro camvas
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
+
+//div donde llegara nuestro canvas
+const btnScanQR = document.getElementById("btn-scan-qr");
+
+//lectura desactivada
+let scanning = false;
+
+//funcion para encender la camara
+function encenderCamara = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function (stream) {
+      scanning = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
+};
+
+//funciones para levantar las funiones de encendido de la camara
+function tick() {
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  scanning && requestAnimationFrame(tick);
+}
+
+function scan() {
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
+  }
+}
+
+//apagara la camara
+const cerrarCamara = () => {
+  video.srcObject.getTracks().forEach((track) => {
+    track.stop();
+  });
+  canvasElement.hidden = true;
+  btnScanQR.hidden = false;
+};
+
+
+//callback cuando termina de leer el codigo QR
+qrcode.callback = (respuesta) => {
+  if (respuesta) {
+    //console.log(respuesta);
+    Swal.fire(respuesta)
+    activarSonido();
+    //encenderCamara();    
+    cerrarCamara();    
+
+  }
+};
+//evento para mostrar la camara sin el boton 
+window.addEventListener('load', (e) => {
+  encenderCamara();
+})
+
+window.addEventListener('load', (e) =>{
+    encenderCamara();
+})
