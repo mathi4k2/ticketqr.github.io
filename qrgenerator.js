65fdf76c12be8462dc2a20628c65fd475c377264
mathi4k2 <mathiforcado@gmail.com>
2023-09-16 12:07:59 -0400

    correcciones

--- qrgenerator.js
+++ qrgenerator.js
@@ -1,13 +1,27 @@
 function generarCodigoQR() {
-    // Obtiene el número de documento ingresado por el usuario
-    const numeroDocumento = document.getElementById('CI').value;
-    // Obtiene el elemento donde se mostrará el código QR
-    const codigoQRDiv = document.getElementById('codigoQR');
+    // Verifica si todos los campos requeridos están llenos
+    const camposRequeridos = document.querySelectorAll('[required]');
+    let todosLlenos = true;
 
-    // Crea el código QR con qrcode.js
-    const qrcode = new QRCode(codigoQRDiv, {
-        text: numeroDocumento, // Datos que se convertirán en QR
-        width: 128, // Ancho del código QR
-        height: 128 // Alto del código QR
+    camposRequeridos.forEach(campo => {
+        if (!campo.value) {
+            todosLlenos = false;
+        }
     });
+
+    if (todosLlenos) {
+        // Todos los campos requeridos están llenos, genera el código QR
+        const numeroDocumento = document.getElementById('CI').value;
+        const codigoQRDiv = document.getElementById('codigoQR');
+        codigoQRDiv.innerHTML = "";
+
+        const qrcode = new QRCode(codigoQRDiv, {
+            text: numeroDocumento,
+            width: 128,
+            height: 128
+        });
+    } else {
+        // No todos los campos requeridos están llenos, muestra un mensaje de error o toma otra acción apropiada
+        alert("Por favor, complete todos los campos requeridos antes de generar el código QR.");
+    }
 }
